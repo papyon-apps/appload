7dc4772dd12598d6be86a752f86092553e1f67e8
asamethon <2403105047@std.idu.edu.tr>
2024-10-08 14:55:26 +0300

    Refactor: Move artifact menu to the right in Builds component

--- src/components/Builds.tsx
+++ src/components/Builds.tsx
@@ -56,43 +56,39 @@ export async function Builds({ artifacts }: Props) {
 
   return (
     <div className="flex flex-col justify-center items-center p-10">
-      <h1 className="text-4xl font-semibold">
-        {artifacts.name || "example-app"}
-      </h1>
-      <Menu as="div" className="relative inline-block text-left mt-10">
-        <div>
-          <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            Other Artifacts
-            <ChevronDownIcon
-              aria-hidden="true"
-              className="-mr-1 h-5 w-5 text-gray-400"
-            />
-          </MenuButton>
-        </div>
-
-        <MenuItems
-          transition
-          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-        >
-          <div className="py-1">
-            {artifactNames.map((name) => (
-              <MenuItem key={name}>
-                <Link
-                  href={`/build/${name}`}
-                  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                >
-                  {name}
-                </Link>
-              </MenuItem>
-            ))}
-          </div>
-        </MenuItems>
-      </Menu>
       <div className="flex flex-row items-center gap-10 mt-10">
         <Link className="border border-white p-2 px-2.5 rounded-sm" href={"/"}>
           <h1>←</h1>
         </Link>
-        <h1 className="text-5xl">Builds </h1>
+        <Menu as="div" className="relative inline-block text-left mt-10">
+          <div>
+            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+              {artifacts.name || "Other Artifacts"}
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="-mr-1 h-5 w-5 text-gray-400"
+              />
+            </MenuButton>
+          </div>
+
+          <MenuItems
+            transition
+            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+          >
+            <div className="py-1">
+              {artifactNames.map((name) => (
+                <MenuItem key={name}>
+                  <a
+                    href={`/build/${name}`}
+                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                  >
+                    {name}
+                  </a>
+                </MenuItem>
+              ))}
+            </div>
+          </MenuItems>
+        </Menu>
       </div>
 
       <div className="flex flex-col lg:flex-row items-center gap-10 mt-10">
